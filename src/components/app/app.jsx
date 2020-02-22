09cdfa2b7142d07015c7ab7984394c828161d91f
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-02-22 20:51:00 +0200

    2.1. Add movie list props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -5,12 +5,15 @@ import Main from "../main/main.jsx";
 const App = (props) => {
   // eslint-disable-next-line react/prop-types
   const {movieName, movieGenre, movieYear} = props;
+  // eslint-disable-next-line react/prop-types
+  const {movies} = props;
 
   return (
     <Main
       movieName={movieName}
       movieGenre={movieGenre}
       movieYear={movieYear}
+      movies={movies}
     />
   );
 };
