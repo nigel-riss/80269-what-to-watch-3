519e12689361e43200bb28516f92bed03d43be0a
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-02-23 05:05:36 +0200

    2.2. Add snapshot tests

---
+++ src/components/main/main.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Main from "./main";
+
+it(`<Main /> renders correctly`, () => {
+  const tree = renderer
+    .create(<Main
+      movieName={`Sci-Fi`}
+      movieGenre={`Infinity`}
+      movieYear={2037}
+      movies={[`2001: A Space Odyssey`, `Interstellar`, `Ad Astra`]}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
